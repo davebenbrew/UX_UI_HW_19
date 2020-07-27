52fa5e0ad1b49b00f6f3cd80a76fedf1d2fc5935
davebenbrew <61244844+davebenbrew@users.noreply.github.com>
2020-07-27 00:08:43 -0600

    website updates

--- js/index.js
+++ js/index.js
@@ -1,53 +1,13 @@
 console.log("js is loaded correctly");
 
-$(".heroText").on("click", function () { 
-    
-    console.log("something something");
-
-});
-
-$(".skillButton").on("click", function() {
-    
+$(".skillButton").on("click", function() { 
     console.log("you clicked my button");
-    $(this).css("background color", "white");
-    
 });
 
-/*
-$(".skillButton").hover( function( event ) {
-  $(event.delegateTarget ).css( "background-color", "green");
-});
-
-
-$('.skillButton').hover(function() {
-  
-  
-
-  if($(this).hasClass('clicked')) {
-    $(this).css( "background-color", "white");
-  }
-
-  else $(this).css( "background-color", "green");
-
-});
-
-$(".skillButton").hover( function() {
-  $(this).css( "background-color", "green");
-});
-
-*/
 
 $(document).ready(function(){
   $(".skillButton").hover(function(){
-    $(this).css("background-color", "white").show(".skillsList")
-      
-      
-      
-      ;
-    
-  
-  
-  },
+    $(this).css("background-color", "white").show(".skillsList");},
     
     function(){
     $(this).css("background-color", "#007556");
